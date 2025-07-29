b643914aa68db3dd8c501833b71b44e633294850
Thành <anhchang00@gmail.com>
2025-07-29 15:35:32 +0700

    Thêm tính năng gửi hoá đơn cho len

--- src/main/java/thanhtrang/online/thanhtrang/SendPostWithHttpClient.java
+++ src/main/java/thanhtrang/online/thanhtrang/SendPostWithHttpClient.java
@@ -17,16 +17,33 @@ public class SendPostWithHttpClient {
         try {
             HttpClient client = HttpClient.newHttpClient();
             HttpRequest request = HttpRequest.newBuilder()
-                    .uri(URI.create("https://nodered.thanhtrang.online/api/notification"))
+                    .uri(URI.create("https://nodered.thanhcom.site/api/notification"))
                     .header("Content-Type", "application/json")
                     .POST(HttpRequest.BodyPublishers.ofString("{\"name\":\"" + c.getName() + "\",\"phone\":\"" + c.getPhone() + "\","
                             + "\"totalpay\":\"" + (r.getTkPrice() + r.getGkPrice()) + "\",\"time\":\"" + r.getDate() + "\"}"))
                     .build();
-
             HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
             System.out.println("Response: " + response.body());
         } catch (IOException | InterruptedException e) {
             throw new RuntimeException(e);
+        }
+    }
+    public static void main(String[] args) {
+        SendPostWithHttpClient client = new SendPostWithHttpClient();
+        EyeService e = new EyeService();
+        e.setEyeid(0);
+        e.setEyeadd(234);
+        Customer C = new Customer();
+        C.setName("Thanh oke");
+        C.setPhone("0962100123");
+       Receipt r = new Receipt();
+       r.setTkPrice(400000);
+       r.setGkPrice(9000);
+       r.setDate("12-12-1991");
+        try {
+            client.SendRequest(e, C, r);
+        } catch (Exception ex) {
+            System.getLogger(SendPostWithHttpClient.class.getName()).log(System.Logger.Level.ERROR, (String) null, ex);
         }
     }
 }
